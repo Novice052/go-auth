325df060e53a1f8ccbfd600dd58e3c5dcaefd94f
Novice052 <immersed101@gmail.com>
2022-03-07 00:29:39 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"./database"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	database.Connect()
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":3000")
+
+}
